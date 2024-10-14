8dde1d5b2d6da05e0dc4ea27b43dc84df9e5d255
elia renzoni <elia.renzoni03@gmail.com>
2024-10-14 18:21:43 +0200

    add cache structure

--- store/store.go
+++ store/store.go
@@ -1,1 +1,34 @@
 package store
+
+import "sync"
+
+type KnucklesDB struct {
+	lock sync.RWMutex
+	tlb  map[string]DBvalues
+}
+
+func NewKnucklesDB() *KnucklesDB {
+	return &KnucklesDB{
+		tlb: make(map[string]DBvalues),
+	}
+}
+
+func (k *KnucklesDB) SetWithIpAddressOnly(address string) (err error) {
+	return
+}
+
+func (k *KnucklesDB) SetWithEndpointOnly(endpoint string) (err error) {
+	return
+}
+
+func (k *KnucklesDB) SearchWithIpOnly(addres string) (err error) {
+	return
+}
+
+func (k *KnucklesDB) SearchWithEndpointOnly(enpoint string) (err error) {
+	return
+}
+
+func (k *KnucklesDB) DeleteEntry(entryID string) (err error) {
+	return
+}
