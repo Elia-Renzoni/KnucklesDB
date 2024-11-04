ec9d458b716fac954470fd0a1e22db8e97686a05
Elia <elia.renzoni03@gmail.com>
2024-11-04 17:57:12 +0100

    update failure_detector_test

--- detector/failure_detector_test.go
+++ detector/failure_detector_test.go
@@ -1,1 +1,51 @@
 package detector_test
+
+
+import (
+	"knucklesdb/detector"
+	"testing"
+)
+
+func TestNewFailureDetector(t *testing.T) {
+	tree := detector.NewDectionBST()
+	if instance := detector.NewFailureDetector(tree); instance == nil {
+		t.Fail()
+	}
+}
+
+func TestFaultDetection(t *testing.T) {
+	dTree := detector.NewDectionBST()
+	dTree.Insert("/foo", 50)
+	dTree.Insert("/foo2", 45)
+	dTree.Insert("/foo3", 40)
+	dTree.Insert("/foo4", 39)
+	dTree.Insert("/foo5", 20)
+
+	faultsD := detector.NewFailureDetector(dTree)
+	faultsD.FaultDetection()
+
+	var removedItemsList []string = make([]string, 3)
+	removedItemsList = append(removedItemsList, "/foo3")
+	removedItemsList = append(removedItemsList, "/foo4")
+	removedItemsList = append(removedItemsList, "/foo5")
+
+	node := dTree.Root
+	var key int16
+	for _, removedItem := range removedItemsList {
+		switch removedItem {
+		case "/foo3":
+			key = 40
+		case "/foo4":
+			key = 39
+		case "/foo5":
+			key = 20
+		}
+		for node != nil && node.GetNodeID() != removedItem {
+			if key < node.GetNodeLogicalClock() {
+				node = node.GetNodeLeftChild()
+			} else {
+				node = node.GetNodeRightChild()
+			}
+		}
+	}
+}
