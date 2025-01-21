2b4d766c62b042d3490e9e40f701b1faf28daeb0
elia renzoni <elia.renzoni03@gmail.com>
2025-01-21 14:09:18 +0100

    bug fixing

--- store/cache_page.go
+++ store/cache_page.go
@@ -2,7 +2,7 @@
 * This file contains the Bucket data structure
 * A Bucket is equivalent to a Page stored in the Buffer Pool
 *
-*/
+ */
 
 package store
 
@@ -13,19 +13,19 @@ import (
 
 const (
 	PAGE_SIZE uint32 = 2024
-	
+
 	// whithin this character is possible to separate
-	// the key from the value. In this way the 
+	// the key from the value. In this way the
 	// retrevail phase can become more speedy
 	// key@value
 	SEPARATOR rune = '@'
 )
 
 type Page struct {
-	
+
 	// the page id is mark out by the index of the array
 	pageID int
-	
+
 	// logical clock increased by the clock module
 	knucklesClock int
 
@@ -38,9 +38,9 @@ type Bucket struct {
 
 func Palloc(logicalClock, bucketID int) *Page {
 	return &Page{
-		pageID: bucketID,
+		pageID:        bucketID,
 		knucklesClock: logicalClock,
-		collisionList: make(Bucket, 0)
+		collisionList: make([]Bucket, 0),
 	}
 }
 
@@ -49,14 +49,14 @@ func (p *Page) AddPage(key, value []byte) {
 	b.bucketData = fillBucket(key, value)
 
 	p.collisionList = append(p.collisionList, b)
-} 
+}
 
 func (p *Page) ReadValueFromBucket(key []byte) (error, []byte) {
 	for index := range p.collisionList {
 		bucket := p.collisionList[index]
-		if result := bytes.Contains(bucket, key); result == true {
-			key, valueToRetrieve, ok := bytes.Cut(bucket, []byte(SEPARATOR))
-			return nil, valueToRetrieve 
+		if result := bytes.Contains(bucket.bucketData[:], key); result == true {
+			_, valueToRetrieve, _ := bytes.Cut(bucket.bucketData[:], []byte("@"))
+			return nil, valueToRetrieve
 		}
 	}
 
@@ -68,16 +68,14 @@ func (p *Page) ReadValueFromBucket(key []byte) (error, []byte) {
 *   @param key
 *	@param value
 *   @return filled bucket
-*/
+ */
 func fillBucket(key, value []byte) (preBucket [PAGE_SIZE]byte) {
-	var buffer := bytes.Buffer{}
+	var buffer = bytes.Buffer{}
 
 	buffer.Write(key)
 	buffer.WriteRune(SEPARATOR)
 	buffer.Write(value)
 
-	copy(preBucket[:], buffer.Bytes)
-	return 
+	copy(preBucket[:], buffer.Bytes())
+	return
 }
-
-
