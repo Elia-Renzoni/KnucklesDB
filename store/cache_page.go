9aca222dddf135c33f6db573861a31e47454864f
Elia <elia.renzoni03@gmail.com>
2025-01-21 10:36:16 +0100

    add buffer pool and page cache

---
+++ store/cache_page.go
@@ -0,0 +1,83 @@
+/**
+* This file contains the Bucket data structure
+* A Bucket is equivalent to a Page stored in the Buffer Pool
+*
+*/
+
+package store
+
+import (
+	"bytes"
+	"errors"
+)
+
+const (
+	PAGE_SIZE uint32 = 2024
+	
+	// whithin this character is possible to separate
+	// the key from the value. In this way the 
+	// retrevail phase can become more speedy
+	// key@value
+	SEPARATOR rune = '@'
+)
+
+type Page struct {
+	
+	// the page id is mark out by the index of the array
+	pageID int
+	
+	// logical clock increased by the clock module
+	knucklesClock int
+
+	collisionList []Bucket
+}
+
+type Bucket struct {
+	bucketData [PAGE_SIZE]byte
+}
+
+func Palloc(logicalClock, bucketID int) *Page {
+	return &Page{
+		pageID: bucketID,
+		knucklesClock: logicalClock,
+		collisionList: make(Bucket, 0)
+	}
+}
+
+func (p *Page) AddPage(key, value []byte) {
+	var b Bucket = Bucket{}
+	b.bucketData = fillBucket(key, value)
+
+	p.collisionList = append(p.collisionList, b)
+} 
+
+func (p *Page) ReadValueFromBucket(key []byte) (error, []byte) {
+	for index := range p.collisionList {
+		bucket := p.collisionList[index]
+		if result := bytes.Contains(bucket, key); result == true {
+			key, valueToRetrieve, ok := bytes.Cut(bucket, []byte(SEPARATOR))
+			return nil, valueToRetrieve 
+		}
+	}
+
+	return errors.New("Cache Miss"), nil
+}
+
+/**
+*	This function fill the bucket with the datas showed as parameters
+*   @param key
+*	@param value
+*   @return filled bucket
+*/
+func fillBucket(key, value []byte) (preBucket [PAGE_SIZE]byte) {
+	var buffer := bytes.Buffer{}
+
+	buffer.Write(key)
+	buffer.WriteRune(SEPARATOR)
+	buffer.Write(value)
+
+	copy(preBucket[:], buffer.Bytes)
+	return 
+}
+
+
--- store/knuckles_buffer_pool.go
+++ store/knuckles_buffer_pool.go
@@ -1,8 +1,38 @@
 package store
 
+import (
+	"errors"
+)
+
+const (
+	MAX_PAGES int = 3000
+)
+
 type BufferPool struct {
+	pages [MAX_PAGES]*Page
 }
 
 func NewBufferPool() *BufferPool {
 	return &BufferPool{}
+}
+
+func (b *BufferPool) WritePage(pageID int, key, value []byte, clock int) {
+	var page *Page = Palloc(clock, key, value, pageID)
+	page.AddPage(key, value)
+	b.pages[pageID] = page
+}
+
+func (b *BufferPool) ReadPage(pageID int, key []byte) (error, []byte) {
+	var page *Page = b.pages[pageID]
+
+	if page == nil {
+		return errors.New("Cache Miss"), nil
+	}
+
+	err, value := page.ReadValueFromBucket(key)
+	return err, value
+}
+
+func (b *BufferPool) EvictPage(pageID int) {
+	// TODO
 }
