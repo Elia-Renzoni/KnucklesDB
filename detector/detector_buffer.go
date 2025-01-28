dc8bd187d6a0abe05775a4265af1afd5ea6a34d7
elia renzoni <elia.renzoni03@gmail.com>
2025-01-28 10:41:35 +0100

    add clock replacement algorithm

--- detector/detector_buffer.go
+++ detector/detector_buffer.go
@@ -1,13 +1,24 @@
+/**
+*	This file contains the implementation of the clock page replacement algorithm.
+*
+**/
 package detector
+
+import (
+	"sync"
+	"time"
+)
 
 type DetectorBuffer struct {
 	buffer     map[string]*Victim
 	bufferSize int
+	wg         *sync.WaitGroup
 }
 
 func NewDetectorBuffer() *DetectorBuffer {
 	return &DetectorBuffer{
-		buffer: make(map[string]*Victim),
+		buffer:     make(map[string]*Victim),
+		bufferSize: 0,
 	}
 }
 
@@ -26,4 +37,28 @@ func (d *DetectorBuffer) SearchPage(v *Victim) bool {
 
 func (d *DetectorBuffer) UpdateVictimEpoch(v *Victim) {
 	d.buffer[string(v.key)] = v
+}
+
+func (d *DetectorBuffer) ClockPageEviction() {
+	for {
+		time.Sleep(3 * time.Second)
+
+		// if the buffer is empty do nothing
+		if d.bufferSize == 0 {
+			continue
+		}
+
+		d.wg.Add(1)
+
+		// loop over the buffer
+		for _, victim := range d.buffer {
+			if victim.epoch {
+				victim.epoch = false
+			} else {
+				// if the page is false then i can remove it
+				// TODO => singular queue buffer per gli update
+			}
+		}
+		d.wg.Done()
+	}
 }
--- detector/singular_update_queue.go
+++ detector/singular_update_queue.go
@@ -1,3 +1,11 @@
+/**
+* This file contains the implementation of the Singular Update Queue pattern,
+*  according to which no locks are used to protect shared resources.
+*  Instead, there is a single goroutine that reads from a channel and updates the information in the shared area itself.
+*
+*  channel [][][][][][][][][] <- reader -> buffer
+ */
+
 package detector
 
 type SingularUpdateQueue struct {
@@ -12,12 +20,25 @@ func NewSingularUpdateQueue(clockBuffer *DetectorBuffer) *SingularUpdateQueue {
 	}
 }
 
+/**
+*  @brief This method must be called by the store to update the buffer with the new entries.
+*  @param page to add
+*
+ */
 func (s *SingularUpdateQueue) AddVictimPage(page *Victim) {
 	s.updateQueue <- page
 }
 
+/**
+*
+* @brief This method implements the core of the goroutine that will read data from the buffer
+*  and update the shared resource.
+*
+ */
 func (s *SingularUpdateQueue) UpdateQueueReader() {
 	for {
+		// wait until the replacement algorithm is done
+		s.detectorBuffer.wg.Wait()
 		select {
 		case victim := <-s.updateQueue:
 			ok := s.detectorBuffer.SearchPage(victim)
