fabefb619dacd2251f5a8b443f91e8fdccc92592
Elia <elia.renzoni03@gmail.com>
2025-02-26 14:46:12 +0100

    update recover source

--- recover/crash_recovery.go
+++ recover/crash_recovery.go
@@ -1,6 +1,5 @@
 package recover
 
-
 import (
 	"knucklesdb/store"
 	"kcnuklesdb/wal"
@@ -8,16 +7,28 @@ import (
 
 type Recover struct {
 	dbState *store.KnucklesMap
-	wal *wal.WAL
+	walAPI *wal.WALLockFreeQueue
 }
 
-func NewRecover(db *store.KnucklesMap, wal *wal.WAL) *Recover {
+func NewRecover(db *store.KnucklesMap, wal *wal.WALLockFreeQueue) *Recover {
 	return &Recover{
 		db: db,
 		wal: wal,
 	}
 }
 
-func (r *Recover) StartRecovery() {
+func (r *Recover) SetOperationWAL(hash int32, key, value []byte) {
+	entry := wal.NewWALEntry(hash, []byte("Set"), key, value)
 
+	r.walAPI.AddEntry(entry)
+}
+
+func (r *Recover) DeleteOperationWAL(hash int32, key, value []byte) {
+	entry := wal.NewWALEntry(hash, []byte("Delete"), key, value)
+
+	r.walAPI.AddEntry(entry)
+}
+
+func (r *Recover) StartRecovery() {
+	// TODO
 }
