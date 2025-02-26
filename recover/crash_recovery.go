e47866468a6d8a2a05c0cb57811c702271548689
Elia <elia.renzoni03@gmail.com>
2025-02-26 10:59:48 +0100

    update

--- recover/crash_recovery.go
+++ recover/crash_recovery.go
@@ -5,3 +5,19 @@ import (
 	"knucklesdb/store"
 	"kcnuklesdb/wal"
 )
+
+type Recover struct {
+	dbState *store.KnucklesMap
+	wal *wal.WAL
+}
+
+func NewRecover(db *store.KnucklesMap, wal *wal.WAL) *Recover {
+	return &Recover{
+		db: db,
+		wal: wal,
+	}
+}
+
+func (r *Recover) StartRecovery() {
+
+}
