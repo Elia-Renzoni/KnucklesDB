6327c658afa1a0d0b0e8f91cb9b0d67f6df9efc1
Elia <elia.renzoni03@gmail.com>
2025-03-04 12:24:41 +0100

    .

--- example/set/client.go
+++ example/set/client.go
@@ -3,11 +3,15 @@ package main
 import (
 	"knucklesdb/client"
 	"time"
+	"fmt"
 )
 
 func main() {
 	knucklesClient := client.NewClient("127.0.0.1", "5050", "127.0.0.1", "5050", 3 * time.Second)
 	
-	go knucklesClient.Set([]byte("/foo"), []byte("192.78.4.1"))
+	go func() {
+		err := knucklesClient.Set([]byte("/foo"), []byte("192.78.4.1"))
+		fmt.Println(err)
+	}()
 	for {}
 }
