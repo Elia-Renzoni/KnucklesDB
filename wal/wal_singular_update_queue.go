cf07830b403cc28725fc96e07b1d8d8b354c6673
Elia <elia.renzoni03@gmail.com>
2025-03-11 16:33:44 +0100

    update wal by adding comments

--- wal/wal_singular_update_queue.go
+++ wal/wal_singular_update_queue.go
@@ -1,3 +1,10 @@
+/**
+*	This file contains the implementation of the singular update queue pattern used in KnucklesDB
+*	to delete all the mutex managers that would force a bottleneck.
+*   The entries are written in an unbounded channel of 100 elements by some store functions and readed 
+*	by the method EntryReader, that will read 5 entry at a time.
+**/
+
 package wal
 
 import (
