392121929fcf8c5df7d7e4f33da67131d26355ab
Elia <elia.renzoni03@gmail.com>
2025-03-11 16:29:00 +0100

    update wal by adding comments

--- wal/wal.go
+++ wal/wal.go
@@ -1,3 +1,10 @@
+/**
+*	This file contains the implementation of the WAL pattern used for acheiving strong durability
+*	WAL is written only if SET and DELETE operations occurs by using the method WriteWAL.
+*	The implementation contains a method for scan (reading) the WAL file and write evry line
+*	to a channel used for the recovery procedure.
+**/
+
 package wal
 
 import (
@@ -20,10 +27,6 @@ type WAL struct {
 	RecoveryChannel chan WALEntry
 }
 
-const (
-	WRITE int = iota * 1
-	READ
-)
 
 func NewWAL() *WAL {
 	return &WAL{
@@ -34,6 +37,10 @@ func NewWAL() *WAL {
 	}
 }
 
+/*
+*	@brief this method allow the database to write the requests in the WAL.
+*	@param encoded entry to write
+**/
 func (w *WAL) WriteWAL(toAppend WALEntry) {
 	var (
 		err          error
@@ -48,11 +55,14 @@ func (w *WAL) WriteWAL(toAppend WALEntry) {
 	}
 	defer w.walFile.Close()
 
+	// get the offset stored in the map for the given key
 	entryOffset, ok := w.walHash[toAppend.Hash]
 	var newLine = bytes.NewBufferString("\n")
 	
+	// prepare the message encoding
 	buffer = [][]byte{toAppend.Method, toAppend.Key, toAppend.Value, newLine.Bytes()}
 	entryToWrite = bytes.Join(buffer, []byte(", "))
+	// check if the entry is already present in the map
 	if ok {
 		w.walFile.WriteAt(entryToWrite, entryOffset)
 	} else {
@@ -63,6 +73,10 @@ func (w *WAL) WriteWAL(toAppend WALEntry) {
 	}
 }
 
+/*
+*	@brief this method check if the WAL file is full
+*	@return boolean value indicating the result of the execution
+**/
 func (w *WAL) IsWALFull() bool {
 	_, err := os.ReadFile("wal.txt")
 	if err != nil {
@@ -72,6 +86,10 @@ func (w *WAL) IsWALFull() bool {
 	return true
 }
 
+/**
+*	@brief this method scans the WAL file and memorize the entries to a channel
+*	used for the recovery session.
+*/
 func (w *WAL) ScanLines() {
 	var err error
 
@@ -97,6 +115,9 @@ func (w *WAL) ScanLines() {
 	close(w.RecoveryChannel)
 }
 
+/*
+*	@brief this method set the offset for the write operation
+**/
 func (w *WAL) setWriteOffset(bytesToWrite []byte) {
 	w.helperBuffer.Write(bytesToWrite)
 
