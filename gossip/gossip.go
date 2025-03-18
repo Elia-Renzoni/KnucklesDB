d639ce2bcfbb791a6563667d8fcca3fae3945c57
Elia <elia.renzoni03@gmail.com>
2025-03-18 10:19:46 +0100

    update gossip

--- gossip/gossip.go
+++ gossip/gossip.go
@@ -4,15 +4,32 @@
 
 package gossip
 
+import (
+	"time"
+)
+
 type GossipProtocol struct {
 	gossipUtils *GossipUtils
+	waitTime func(time.Duration)
 }
 
-func NewGossipProtocol() *GossipProtocol {
-	return &GossipProtocol{}
+func NewGossipProtocol(gossip *GossipUtils, interval time.Duration) *GossipProtocol {
+	return &GossipProtocol{
+		gossipUtils: gossip,
+		waitTime: func(interval) {
+			time.Sleep(interval)
+		},
+	}
 }
 
-func (g *GossipProtocol) StartGossipRound() {
+func (g *GossipProtocol) StartGossip() {
+	for {
+		g.waitTime()
+
+		// TODO
+		g.gossipUtils.Send()
+		
+	}
 }
 
 func (g *GossipProtocol) SpreadMembershipList(fanout []string) {
--- gossip/gossip_utils.go
+++ gossip/gossip_utils.go
@@ -1,7 +1,12 @@
 package gossip
 
+import (
+	"net"
+	"context"
+)
 
 type GossipUtils struct {
+	gossipGlobalContext context.Context
 }
 
 func NewGossipUtils() *GossipUtils {
@@ -10,4 +15,8 @@ func NewGossipUtils() *GossipUtils {
 
 func (g *GossipUtils) Send(nodeAddress string) {
 
+	conn, err := net.Dial("tcp", nodeAddress)
+	if err != nil {
+		return
+	}
 }
