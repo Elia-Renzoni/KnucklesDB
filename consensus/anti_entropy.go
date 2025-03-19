40683fa7e9e348433534316ed015f1e5e4056c75
Elia <elia.renzoni03@gmail.com>
2025-03-19 10:30:31 +0100

    update anti-entropy

--- consensus/anti_entropy.go
+++ consensus/anti_entropy.go
@@ -4,21 +4,30 @@ import (
 	"knucklesdb/wal"
 	"knucklesdb/gossip"
 	"knucklesdb/swim"
+	"time"
 )
-
 
 type AntiEntropy struct {
 	gossipProtocol *gossip.GossipProtocol
 	membershipList *swim.ClusterManager
+	infoLogger *wal.InfoLogger
+	sleepTime func(time.Duration)
 }
 
-func NewAntiEntropy(gProtocol *gossip.GossipProtocol, clusterList *swim.ClusterManager) *AntiEntropy {
+func NewAntiEntropy(gProtocol *gossip.GossipProtocol, clusterList *swim.ClusterManager, logger *wal.InfoLogger) *AntiEntropy {
 	return &AntiEntropy{
 		gossipProtocol: gProtocol,
 		membershipList: clusterList,
+		infoLogger: logger,
+		sleepTime: func(frequency time.Duration) {
+			time.Sleep(frequency)
+		},
 	}
 }
 
 func (a *AntiEntropy) ScheduleAntiEntropy() {
-
+	a.infoLogger.ReportInfo("Anti-Entropy Routine ON")
+	for {
+		a.sleepTime(1000 * time.Milliseconds)
+	}
 }
