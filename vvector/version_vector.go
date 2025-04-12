48310146b39bc278431c561b7d2fc031300ff830
elia renzoni <elia.renzoni03@gmail.com>
2025-04-12 14:32:54 +0200

    add version vector struct

--- vvector/version_vector.go
+++ vvector/version_vector.go
@@ -1,2 +1,21 @@
 package vvector
 
+import (
+	id "github.com/google/uuid"
+)
+
+type VersionVector struct {
+	nodeID      id.UUID
+	dataVersion int64
+}
+
+func NewVersionVector(node id.UUID) VersionVector {
+	return VersionVector{
+		nodeID:      node,
+		dataVersion: 0,
+	}
+}
+
+func (v *VersionVector) IncrementVector() {
+	v.dataVersion += 1
+}
