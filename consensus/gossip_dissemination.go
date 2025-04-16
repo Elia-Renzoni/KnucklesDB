a20b745c517bcb438818d3620fcc96ca1f5034a9
Elia <elia.renzoni03@gmail.com>
2025-04-16 17:01:22 +0200

    update gossip

--- consensus/gossip_dissemination.go
+++ consensus/gossip_dissemination.go
@@ -1,12 +1,21 @@
 package consensus
 
-type Gossip struct {
+import (
+	"net"
+	"context"
+)
 
+type Gossip struct {
+	gossipConn net.Conn
 }
 
 
 func NewGossip() *Gossip {
 	return &Gossip{}
+}
+
+func (g *Gossip) send(host, port string) {
+
 }
 
 
